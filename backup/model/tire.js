01455827f695898961653682f7244123b99e2b95
ezacoantonio <126419692+ezacoantonio@users.noreply.github.com>
2024-01-02 18:55:27 -0500

    Thread and Set update

--- backup/model/tire.js
+++ backup/model/tire.js
@@ -3,10 +3,11 @@ const mongoose = require('mongoose');
 const tireSchema = new mongoose.Schema({
   brand: String,
   size: String,
-  treadCondition: String,
+  treadCondition: { type: String, enum: ['10%', '20%', '30%', '40%', '50%', '60%', '70%', '80%', '90%', '100%'] },
   status: { type: String, enum: ['posted', 'not posted', 'sold', 'not sold'] },
   imageUrl: String ,// Changed from imageBase64 to imageUrl
-  location: { type: String, enum: ['Toronto', 'Barrie', 'Sutton West'] }
+  location: { type: String, enum: ['Toronto', 'Barrie', 'Sutton West'] },
+  setInfo: { type: String, enum: ['Set of 2', 'Set of 4'] }
 });
 
 module.exports = mongoose.model('Tire', tireSchema);
