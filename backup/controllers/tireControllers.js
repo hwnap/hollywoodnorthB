c1873ec747eb4181742e1431512938c85dc11e7c
ezacoantonio <126419692+ezacoantonio@users.noreply.github.com>
2024-01-02 20:46:36 -0500

    Image update 1

--- backup/controllers/tireControllers.js
+++ backup/controllers/tireControllers.js
@@ -1,4 +1,5 @@
-const Tire = require('../../backup/model/tire');
+// controllers/tireControllers.js
+const Tire = require('../model/tire'); // Update the path as needed
 
 exports.getAllTires = async (req, res) => {
   try {
@@ -13,71 +14,12 @@ exports.addTire = async (req, res) => {
   try {
     const newTire = new Tire(req.body);
     await newTire.save();
-    res.status(201).send(newTire);
+    res.status(201).json(newTire);
   } catch (error) {
-    res.status(500).send(error.message);
+    res.status(500).json({ message: error.message });
   }
 };
 
-exports.findTiresBySize = async (req, res) => {
-    try {
-      const size = req.query.size;
-      const tires = await Tire.find({ size: size });
-      res.json(tires);
-    } catch (error) {
-      res.status(500).send(error.message);
-    }
-  };
-  
-  exports.searchTires = async (req, res) => {
-    try {
-      const query = {};
-      if (req.query.size) {
-        query.size = req.query.size;
-      }
-      if (req.query.brand) {
-        query.brand = req.query.brand;
-      }
-  
-      const tires = await Tire.find(query);
-      res.json(tires);
-    } catch (error) {
-      res.status(500).send(error.message);
-    }
-  };
-  
-  // Additional function to update the tire's status and remove the image if sold
-exports.updateTireStatus = async (req, res) => {
-    try {
-      const tireId = req.params.id;
-      const update = { status: req.body.status };
-  
-      if (req.body.status === 'sold') {
-        update.imageBase64 = ''; // Remove the image for sold items
-      }
-  
-      const updatedTire = await Tire.findByIdAndUpdate(tireId, update, { new: true });
-      res.json(updatedTire);
-    } catch (error) {
-      res.status(500).send(error.message);
-    }
-  };
-
-  // In your tireControllers.js or equivalent file
-exports.deleteTire = async (req, res) => {
-  try {
-      const tireId = req.params.id;
-      const tire = await Tire.findByIdAndDelete(tireId);
-      if (!tire) {
-          return res.status(404).send('Tire not found');
-      }
-      res.status(200).send('Tire deleted');
-  } catch (error) {
-      res.status(500).send(error.message);
-  }
-};
-
-// In your controllers/tireControllers.js file
 exports.updateTire = async (req, res) => {
   try {
     const tireId = req.params.id;
@@ -91,3 +33,56 @@ exports.updateTire = async (req, res) => {
     res.status(500).send(error.message);
   }
 };
+
+exports.deleteTire = async (req, res) => {
+  try {
+    const tireId = req.params.id;
+    await Tire.findByIdAndDelete(tireId);
+    res.status(200).send('Tire deleted');
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+exports.searchTires = async (req, res) => {
+  try {
+    const query = {};
+    if (req.query.size) query.size = req.query.size;
+    if (req.query.brand) query.brand = req.query.brand;
+    // Add more query parameters as needed
+
+    const tires = await Tire.find(query);
+    res.json(tires);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+// Other functions you provided earlier
+exports.findTiresBySize = async (req, res) => {
+  try {
+    const size = req.query.size;
+    const tires = await Tire.find({ size });
+    res.json(tires);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+exports.updateTireStatus = async (req, res) => {
+  try {
+    const tireId = req.params.id;
+    const update = { status: req.body.status };
+
+    if (req.body.status === 'sold') {
+      update.imageBase64 = ''; // Remove the image for sold items
+    }
+
+    const updatedTire = await Tire.findByIdAndUpdate(tireId, update, { new: true });
+    res.json(updatedTire);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+module.exports = exports;
